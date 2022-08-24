5b4c92e0f72255ca836da73f117ed15b543a7cfc
wonjin <wonjine7@naver.com>
2022-08-24 21:49:25 +0900

    onKeyPress 이벤트 핸들링

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -22,6 +22,12 @@ class EventPractice extends Component {
         });
       }
 
+      handleKeyPress = (e) => {
+        if(e.key === 'Enter') {
+          this.handleClick();
+        }
+      }
+
   render() {
     return (
       <div>
@@ -32,6 +38,7 @@ class EventPractice extends Component {
           placeholder="사용자명"
           value={this.state.username}
           onChange={this.handleChange}
+          
         />
 
         <input
@@ -40,6 +47,8 @@ class EventPractice extends Component {
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
+
           />
           <button onClick={this.handleClick}>확인</button>
         </div>
