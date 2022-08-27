1d32b1fa2b130381dcdbb2e48e7ad1d05ce4c3c4
wonjin <wonjine7@naver.com>
2022-08-27 21:56:38 +0900

    데이터 추가 기능 완성

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import EventPractice from './EventPractice';
- 
-const App = () => {
-  return <EventPractice />;
-};
- 
+import IterationSample from './IterationSample';
+
+
+class App extends Component {
+  render() {
+    return (
+      <IterationSample/>
+    );
+  }
+}
+
+
+
 export default App;
