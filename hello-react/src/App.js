0ec5f6d9d9d389f94215cb326e3f07efedb59dc0
wonjin <wonjine7@naver.com>
2022-08-30 22:54:00 +0900

    useState로 카운터 구현하기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,42 +1,7 @@
-import React, { Component } from 'react';
-import LifeCycleSample from './LifeCycleSample';
-import ErrorBoundary from './ErrorBoundary';
-
-
-// 랜덤 색상을 생성합니다.
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-
-
-class App extends Component {
-  state = {
-    color: '#000000'
-  };
-
-
-
-handleClick = () => {
-    this.setState({
-      color: getRandomColor()
-    });
-  };
-
-
-
-render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
-
-
-
+import React from 'react';
+import Counter from './Counter';
+ 
+const App = () => {
+  return <Counter />;
+};
 export default App;
