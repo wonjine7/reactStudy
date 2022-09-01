9dd48a36566d44738f48f0695e09c39ae285c7e1
wonjin <wonjine7@naver.com>
2022-09-01 16:46:06 +0900

    등록 버튼 누른 후 포커스 이동

--- hello-react/src/Average.js
+++ hello-react/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback, useRef } from 'react';
 
 
 const getAverage = numbers => {
@@ -13,17 +13,19 @@ const getAverage = numbers => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState('');
+  const inputEl = useRef(null);
 
 
 
-const onChange = e => {
+const onChange = useCallback(e => {
     setNumber(e.target.value);
-  };
-  const onInsert = () => {
+  }, []); // 컴포넌트가 처음 렌더링될 때만 함수 생성
+const onInsert = useCallback(() => {
     const nextList = list.concat(parseInt(number));
     setList(nextList);
     setNumber('');
-  };
+    inputEl.current.focus();
+  }, [number, list]); // number 혹은 list가 바뀌었을 때만 함수 생성
 
 
 
@@ -33,7 +35,7 @@ const avg = useMemo(() => getAverage(list), [list]);
 
 return (
     <div>
-      <input value={number} onChange={onChange} />
+      <input value={number} onChange={onChange} ref={inputEl} />
       <button onClick={onInsert}>등록</button>
       <ul>
         {list.map((value, index) => (
