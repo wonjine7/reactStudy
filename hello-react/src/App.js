4957d1e25fc7990ece421cbc3ed552f1ce3923d8
wonjin <wonjine7@naver.com>
2022-09-10 20:44:56 +0900

    immer의 속성과 useState의 함수형 업데이트를 함께 활용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,17 +1,64 @@
-import React, { Component } from 'react';
-import CSSModule from './CSSModule';
-
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <CSSModule />
-      </div>
+import React, { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
+ 
+const App = () => {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: '', username: '' });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null
+  });
+ 
+  // input 수정을 위한 함수
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    setForm(
+      produce(draft => {
+        draft[name] = value;
+      })
     );
-  }
-}
-
-
-
+  }, []);
+ 
+  // form 등록을 위한 함수
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username
+      };
+ 
+      // array에 새 항목 등록
+      setData(
+        produce(draft => {
+          draft.array.push(info);
+        })
+      );
+ 
+      // form 초기화
+      setForm({
+        name: '',
+        username: ''
+      });
+      nextId.current += 1;
+    },
+    [form.name, form.username]
+  );
+ 
+  // 항목을 삭제하는 함수
+  const onRemove = useCallback(
+    id => {
+      setData(
+        produce(draft => {
+          draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+        })
+      );
+    },
+    []
+  );
+ 
+  return (...);
+};
+ 
 export default App;
